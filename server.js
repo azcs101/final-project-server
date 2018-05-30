d14b13e262aeaf6c467481f732f68a07d32ceae0
Gasim Gasimzada <gasim@appristas.io>
2018-05-31 01:49:12 +0400

    Add new movie request with upload

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.resolve(__dirname, 'posters'))
+    },
+    filename: (req, file, cb) => {
+        console.log(file);
+        cb(null, `${Date.now()}-${file.originalname}`)
+    }
+});
+
+const upload = multer({ storage });
+
 const fs = require('fs');
 
 let data = [];
 try {
-    data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+    data = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'data.json'), 'utf8'));
 } catch (e) {
     data = [];
 }
@@ -12,6 +27,8 @@ try {
 const pageSize = 5;
 
 const app = express(bodyParser.urlencoded({ extended: true }));
+
+app.use('/posters', express.static(path.resolve(__dirname, 'posters')));
 
 app.get('/', (req, res) => {
     let page = req.query.page && req.query.page > 0 ? parseInt(req.query.page) : 1;
@@ -32,6 +49,62 @@ app.get('/', (req, res) => {
         data: pagedData,
         pagination
     });
+});
+
+app.post('/', upload.single('poster'), (req, res) => {
+    const { title, year, plot, genre } = req.body;
+    const file = req.file;
+
+    try {
+        if (!title) {
+            throw 'No title';
+        }
+
+        if (!year) {
+            throw 'No year';
+        }
+
+        if (!plot) {
+            throw 'No plot';
+        }
+
+        if (!genre) {
+            throw 'No genre';
+        }
+
+        if (!file) {
+            throw 'No poster';
+        }
+
+        const newData = {
+            id: data[data.length - 1].id + 1, title, year, plot, genre, poster: `/posters/${file.filename}`
+        };
+
+        data.push(newData);
+
+        res.send({
+            id: newData.id
+        });
+    } catch (error) {
+        res.status(400);
+        res.send({ error });
+    }
+});
+
+app.get('/:id', (req, res) => {
+
+    res.send({});
+});
+
+
+app.put('/:id', (req, res) => {
+
+    res.send({});
+});
+
+app.delete('/:id', (req, res) => {
+
+    res.send({});
 });
 
 app.listen(8080, () => {
